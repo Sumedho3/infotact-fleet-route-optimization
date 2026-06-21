e93bd7b5800f9122eb805a83fab176393e5a2b57
Sowmya <sowmyatammali2005@gmail.com>
2026-06-22 01:00:14 +0530

    refactor: map child tasks collection inside parent routes

--- fleet-route-optimization-service/src/main/java/com/infotact/fleet/entity/Route.java
+++ fleet-route-optimization-service/src/main/java/com/infotact/fleet/entity/Route.java
@@ -1,12 +1,21 @@
 package com.infotact.fleet.entity;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "routes")
